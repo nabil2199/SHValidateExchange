0086fd1e829745b7f2bac6957ea1dd3110f2b986
nabil2199 <nabil2199@gmail.com>
2016-12-27 14:11:24 +0100

    validation

--- SHValidateExchange.ps1
+++ SHValidateExchange.ps1
@@ -5,3 +5,116 @@ Nabil LAKHNACHFI
 OCWS
 #>
 
+
+
+$strUpn = Read-Host "What is the email address to Validate?"
+if (!$strUpn.Contains('@'))
+{
+    CleanupAndFail "$strUpn is not a valid email address"
+}
+$strExServer = Read-Host "Enter the exchange server DNS name? "
+$credEx = Get-Credential -Message "Please provide exchange user credentials"
+
+$mailbox = $null
+try 
+{
+    $mailbox = Get-Mailbox -Identity $strUpn
+} 
+catch
+{   
+}
+
+if (!$mailbox)
+{
+    "Account exists check failed. Unable to find the mailbox for $strUpn - please make sure the Exchange account exists on $strExServer"
+}
+
+$exchange = Get-ExchangeServer
+if (!$exchange.IsE14OrLater)
+{
+    CleanupAndFail "A compatible exchange server version was not found. Please use at least exchange 2010."
+}
+
+$strAlias = $mailbox.Alias
+$strDisplayName = $mailbox.DisplayName
+
+$strLinkedAccount = $strLinkedDomain = $strLinkedUser = $strLinkedServer = $null
+
+function Validate()
+{
+    Param(
+        [string]$Test,
+        [bool]  $Condition,
+        [string]$FailureMsg,
+        [switch]$WarningOnly
+    )
+
+    Write-Host -NoNewline -ForegroundColor White $Test.PadRight(100,'.')
+    if ($Condition)
+    {
+        Write-Host -ForegroundColor Green "Passed"
+        $global:iTotalPasses++
+    }
+    else
+    {
+        if ($WarningOnly)
+        {
+            Write-Host -ForegroundColor Yellow ("Warning: "+$FailureMsg)
+            $global:iTotalWarnings++
+        }
+        else
+        {
+            Write-Host -ForegroundColor Red ("Failed: "+$FailureMsg)
+            $global:iTotalFailures++
+        }
+    }
+}
+
+
+## Exchange ##
+
+Validate -WarningOnly -Test "The mailbox $strUpn is enabled as a room account" -Condition ($mailbox.RoomMailboxAccountEnabled -eq $True) -FailureMsg "RoomMailboxEnabled - without a device account, the Surface Hub will not be able to use various key features."
+$calendarProcessing = Get-CalendarProcessing -Identity $strUpn -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
+Validate -Test "The mailbox $strUpn is configured to accept meeting requests" -Condition ($calendarProcessing -ne $null -and $calendarProcessing.AutomateProcessing -eq 'AutoAccept') -FailureMsg "AutomateProcessing - the Surface Hub will not be able to send mail or sync its calendar."
+Validate -WarningOnly -Test "The mailbox $strUpn will not delete meeting comments" -Condition ($calendarProcessing -ne $null -and !$calendarProcessing.DeleteComments) -FailureMsg "DeleteComments - the Surface Hub may be missing some meeting information on the welcome screen and Skype."
+Validate -WarningOnly -Test "The mailbox $strUpn keeps private meetings private" -Condition ($calendarProcessing -ne $null -and !$calendarProcessing.RemovePrivateProperty) -FailureMsg "RemovePrivateProperty - the Surface Hub will make show private meetings."
+Validate -Test "The mailbox $strUpn keeps meeting subjects" -Condition ($calendarProcessing -ne $null -and !$calendarProcessing.DeleteSubject) -FailureMsg "DeleteSubject - the Surface Hub will not keep meeting subject information."
+Validate -WarningOnly -Test "The mailbox $strUpn does not prepend meeting organizers to subjects" -Condition ($calendarProcessing -ne $null -and !$calendarProcessing.AddOrganizerToSubject) -FailureMsg "AddOrganizerToSubject - the Surface Hub will not display meeting subjects as intended."
+
+#ActiveSync
+$casMailbox = Get-Casmailbox $strUpn -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
+Validate -Test "The mailbox $strUpn has a mailbox policy" -Condition ($casMailbox -ne $null) -FailureMsg "DevicePasswordEnabled - unable to find policy - the Surface Hub will not be able to send mail or sync its calendar."
+
+if ($casMailbox)
+{
+    $policy = $null
+    if ($exchange.IsE15OrLater)
+    {
+        $strPolicy = $casMailbox.ActiveSyncMailboxPolicy
+        $policy = Get-MobileDeviceMailboxPolicy -Identity $strPolicy -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
+        Validate -Test "The policy $strPolicy does not require a device password" -Condition ($policy.PasswordEnabled -ne $True) -FailureMsg "DevicePasswordEnabled - policy requires a device password - the Surface Hub will not be able to send mail or sync its calendar."
+    }
+    else
+    {
+        $strPolicy = $casMailbox.ActiveSyncMailboxPolicy
+        $policy = Get-ActiveSyncMailboxPolicy -Identity $strPolicy -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
+        Validate -Test "The policy $strPolicy does not require a device password" -Condition ($policy.DevicePasswordEnabled -ne $True) -FailureMsg "DevicePasswordEnabled - policy requires a device password - the Surface Hub will not be able to send mail or sync its calendar."
+    }
+    
+    if ($policy -ne $null)
+    {
+        Validate -Test "The policy $strPolicy allows non-provisionable devices" -Condition ($policy.AllowNonProvisionableDevices -eq $null -or $policy.AllowNonProvisionableDevices -eq $true) -FailureMsg "AllowNonProvisionableDevices - policy will not allow the SurfaceHub to sync"
+    }
+    
+}
+
+# Check the default access level
+$orgSettings = Get-ActiveSyncOrganizationSettings
+$strDefaultAccessLevel = $orgSettings.DefaultAccessLevel
+Validate -Test "ActiveSync devices are allowed" -Condition ($strDefaultAccessLevel -eq 'Allow') -FailureMsg "DeviceType Windows Mail is accessible - devices are not allowed by default - the surface hub will not be able to send mail or sync its calendar."
+
+# Check if there exists a device access rule that bans the device type Windows Mail
+$blockingRules = Get-ActiveSyncDeviceAccessRule | where {($_.AccessLevel -eq 'Block' -or $_.AccessLevel -eq 'Quarantine') -and $_.Characteristic -eq 'DeviceType'-and $_.QueryString -eq 'WindowsMail'}
+Validate -Test "Windows mail devices are not blocked or quarantined" -Condition ($blockingRules -eq $null -or $blockingRules.Length -eq 0) -FailureMsg "DeviceType Windows Mail is accessible - devices are blocked or quaratined - the surface hub will not be able to send mail or sync its calendar."
+
+## End Exchange ##
